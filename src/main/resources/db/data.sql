0cb03ace3d43b38dd074eaaa0688c0ab7a7d6fe1
josohyunn <hyun980110@naver.com>
2023-09-05 12:36:15 +0900

    더미데이터 수정

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -1,24 +1,24 @@
 -- user_tb 더미데이터--------------------------------------
 
 insert into user_tb(login_id, password, username, email, address, address2, tel, birth, bizname, usercode) values('ssar', '1234', '쌀킴','ssar@nate.com', '부산시', '부산진구', '010-1234-5677', '1997-02', null, 1);
+insert into user_tb(login_id, password, username, email, address, address2, tel, birth, bizname, usercode) values('cos', '1234', '코드킴','cos@nate.com', '서울시', '광진구', '010-1234-5678', '1997-03', null, 1);
+insert into user_tb(login_id, password, username, email, address, address2, tel, birth, bizname, usercode) values('love', '1234', '쌀킴','love@nate.com', '부산시', '부산진구', '010-1234-5677', '1997-02', null, 1);
+
+insert into user_tb(login_id, password, username, email, address, address2, tel, birth, bizname, usercode) values('samsung', '1234', '담당자1','samsung@nate.com', '부산시', '부산진구', '010-1234-5677', '1997-02', '삼성', 2);
+insert into user_tb(login_id, password, username, email, address, address2, tel, birth, bizname, usercode) values('lg', '1234', '담당자2','lg@nate.com', '부산시', '부산진구', '010-1234-5677', '1997-02', '엘지', 2);
+insert into user_tb(login_id, password, username, email, address, address2, tel, birth, bizname, usercode) values('kakao', '1234', '담당자3','kakao@nate.com', '부산시', '부산진구', '010-1234-5677', '1997-02', '카카오', 2);
 
 
 -- resume_tb 더미데이터------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-insert into resume_tb(title, semi_content, content, education, school, major, career1, career_period1, career_period1_1, career2, career_period2, career_period2_1, career3, career_period3, career_period3_1, open, etc1, etc2, etc3, etc_period1, etc_period2, etc_period3, link1, link2, link3, workField, user_id) 
-values('내일부터 당장 일할 수 있습니다!', '간단 소개글입니다.', '안녕하십니까, 저는 동의대학교를 졸업하였고, 경력은 없으며 나이는 28세 입니다. 비록 비전공자이지만 동영상 강의로 이론을 배우고, 인터넷으로 스터디를 모집하여서 프로젝트를 통해 직접 코딩을 해볼 수 있었습니다. 저는 정말 착하고 순하며, 하라는 일은 모두 다 할수있습니다. 저를 뽑아주세요ㅎㅎ', '대학교 졸업', '동의대학교', '컴퓨터공학과', '신입', null, null, null, null, null, null, null, null, false, '없음', '없음', '없음', null, null, null,'http://www.naver.com' ,null ,null, '백엔드 개발자', 1);
+insert into resume_tb(title, semi_content, content, education, school, major, career1, career_period1, career_period1_1, career2, career_period2, career_period2_1, career3, career_period3, career_period3_1, open, etc1, etc2, etc3, etc_period1, etc_period2, etc_period3, link1, link2, link3, work_field, user_id) 
+values('내일부터 당장 일할 수 있습니다!', '간단 소개글입니다.', '안녕하십니까, 저는 동의대학교를 졸업하였고, 경력은 없으며 나이는 28세 입니다. 비록 비전공자이지만 동영상 강의로 이론을 배우고, 인터넷으로 스터디를 모집하여서 프로젝트를 통해 직접 코딩을 해볼 수 있었습니다. 저는 정말 착하고 순하며, 하라는 일은 모두 다 할수있습니다. 저를 뽑아주세요ㅎㅎ', '대학교 졸업', '동의대학교', '컴퓨터공학과', '신입', null, null, null, null, null, null, null, null, false, '없음', '없음', '없음', null, null, null,'http://www.naver.com', null, null, '백엔드 개발자', 1);
 
-insert into resume_tb(title, semi_content, content, education, school, major, career1, career_period1, career_period1_1, career2, career_period2, career_period2_1, career3, career_period3, career_period3_1, open, etc1, etc_period1, etc2, etc_period2, etc3, etc_period3, link1, link2, link3, workField, user_id) 
+insert into resume_tb(title, semi_content, content, education, school, major, career1, career_period1, career_period1_1, career2, career_period2, career_period2_1, career3, career_period3, career_period3_1, open, etc1, etc_period1, etc2, etc_period2, etc3, etc_period3, link1, link2, link3, work_field, user_id) 
 values('저는 학점이 4.5인 만점자입니다!', 'Qㅃ간단한닥나단ㄴ', '안녕하십니까, 저는 동의대학교 광고홍보학과를 졸업하여 광고를 제작하고 싶습니다. 이론은 물론이거와 독특한 생각들과 창의적인 아이디어로 이제껏 낸 프로젝트들이 수없이 많습니다. 제 git주소로 들어가보시면 많은 결과물을 보실 수 있을 것입니다.', '대학교 졸업', '동의대학교', '광고홍보학과', '네이버', '2020-05-05', '2022-01-05', '배달의 민족', '2018-06-01', '2019-07-11', null, null, null, false, '광고부문 우수상', '2021-04-30', null, null, null, null,'http://www.nancheonjae.com' ,'http://www.블로그광고.com', null, '광고제작 및 홍보', 2);
 
-insert into resume_tb(title, semi_content, content, education, school, major, career1, career_period1, career_period1_1, career2, career_period2, career_period2_1, career3, career_period3, career_period3_1, open, etc1, etc2, etc3, etc_period1, etc_period2, etc_period3, link1, link2, link3, workField, user_id) 
-values('저는 서울대학교 재학중인 학생입니다.', '-서울대생-', '저는 서울대생입니다.','안녕하십니까, 저는 제목으로만 말하겠습니다. 이상입니다.', '대학교 졸업', '서울대학교', '컴퓨터공학과', '신입', null, null, null, null, null, null, null, null, false, '없음', '없음', '없음', null, null, null,'http://www.naver.com' ,null ,null, '백엔드 개발자', 3);
-
-
--- biz_tb 더미데이터------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-insert into biz_tb(login_id, password, bizname, manager_name, manager_tel, manager_email, address) 
-values('samsung', '1234',  '삼성', '최제인', '010-1234-5678','ss@naver.com', '수원 영통구');
+insert into resume_tb(title, semi_content, content, education, school, major, career1, career_period1, career_period1_1, career2, career_period2, career_period2_1, career3, career_period3, career_period3_1, open, etc1, etc2, etc3, etc_period1, etc_period2, etc_period3, link1, link2, link3, work_field, user_id) 
+values('저는 서울대학교 재학중인 학생입니다.', '-서울대생-','안녕하십니까, 저는 제목으로만 말하겠습니다. 이상입니다.', '대학교 졸업', '서울대학교', '컴퓨터공학과', '신입', null, null, null, null, null, null, null, null, false, '없음', '없음', '없음', null, null, null,'http://www.naver.com', null, null, '백엔드 개발자', 3);
 
 
 -- tech_tb 더미데이터------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -45,9 +45,9 @@ insert into tech_tb(techname) values('C#');
 
 -- tech_resume_tb 더미데이터------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-insert into user_tb(resume_id, tech_id) values('1', '1');
-insert into user_tb(resume_id, tech_id) values('1', '4');
-insert into user_tb(resume_id, tech_id) values('2', '3');
-insert into user_tb(resume_id, tech_id) values('2', '6');
-insert into user_tb(resume_id, tech_id) values('2', '7');
-insert into user_tb(resume_id, tech_id) values('3', '3');
+insert into tech_resume_tb(resume_id, tech_id) values(1, 1);
+insert into tech_resume_tb(resume_id, tech_id) values(1, 4);
+insert into tech_resume_tb(resume_id, tech_id) values(2, 3);
+insert into tech_resume_tb(resume_id, tech_id) values(2, 6);
+insert into tech_resume_tb(resume_id, tech_id) values(2, 7);
+insert into tech_resume_tb(resume_id, tech_id) values(3, 3);
