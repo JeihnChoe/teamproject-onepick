bc648c417e3aec568b794b99fcf72b81356e2f03
josohyunn <hyun980110@naver.com>
2023-09-07 20:27:44 +0900

    이력서 삭제 완료 / controller에 update 쪼금 적음

--- src/main/java/shop/mtcoding/teamprojectonepick/resume/ResumeRequestDTO.java
+++ src/main/java/shop/mtcoding/teamprojectonepick/resume/ResumeRequestDTO.java
@@ -36,5 +36,36 @@ public interface ResumeRequestDTO {
         String workField;
         MultipartFile resumeImg;
     }
-    
+
+    @Data
+    public class UpdateDTO {
+        String title;
+        String semiContent;
+        String content;
+        String education;
+        String school;
+        String major;
+        String career1;
+        String careerPeriodS1;
+        String careerPeriodE1;
+        String career2;
+        String careerPeriodS2;
+        String careerPeriodE2;
+        String career3;
+        String careerPeriodS3;
+        String careerPeriodE3;
+        String open;
+        String etc1;
+        String etc2;
+        String etc3;
+        String etcPeriod1;
+        String etcPeriod2;
+        String etcPeriod3;
+        String link1;
+        String link2;
+        String link3;
+        String workField;
+        MultipartFile resumeImg;
+    }
+
 }
