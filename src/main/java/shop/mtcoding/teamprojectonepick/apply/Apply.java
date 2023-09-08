4d0c7d8850ca6305743807c50144e99c7389e4a3
hayula <z2poter@naver.com>
2023-09-08 15:40:01 +0900

    잠깐멈춰

--- src/main/java/shop/mtcoding/teamprojectonepick/apply/Apply.java
+++ src/main/java/shop/mtcoding/teamprojectonepick/apply/Apply.java
@@ -1,28 +1,37 @@
 package shop.mtcoding.teamprojectonepick.apply;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import shop.mtcoding.teamprojectonepick.notice.Notice;
+import shop.mtcoding.teamprojectonepick.resume.Resume;
 
 @NoArgsConstructor
 @Data
 @Table(name = "apply_tb")
 @Entity
 public class Apply {
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Integer id;
 
-    @Column(nullable = false, length = 10000)
-    Integer resumeId;
+    @ManyToOne
+    @JoinColumn(name = "resume_id")
+    private List<Resume> resumeList = new ArrayList<>();
 
-    @Column(nullable = false, length = 10000)
-    Integer noticeId;
+    @ManyToOne
+    @JoinColumn(name = "notice_id")
+    private List<Notice> noticeList = new ArrayList<>();
 }
